10acf0eadd9cbdded3b1b62d9a49e5f86e09b608
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2015-09-24 21:30:34 -0500

    Fixed indentation

--- server.js
+++ server.js
@@ -25,20 +25,20 @@ server.on('clientConnected', function (client) {
 	console.log('Client connected: ', client.id);
 });
 
-server.on('clientDisconnected', function(client) {
-  console.log('clientDisconnected : ', client.id);
+server.on('clientDisconnected', function (client) {
+	console.log('clientDisconnected : ', client.id);
 });
 
-server.on('published', function(packet, client) {
-  console.log('Published MIO: ', packet.payload.toString());
+server.on('published', function (packet, client) {
+	console.log('Published MIO: ', packet.payload.toString());
 });
 
 server.on('ready', setup);
 
-function setup () {
+function setup() {
 	console.log('Mosca is up and running at port 1883');
-	setInterval(function(){
-		server.publish(message, function(){
+	setInterval(function () {
+		server.publish(message, function () {
 			console.log('Published message');
 		});
 	}, 5000);
